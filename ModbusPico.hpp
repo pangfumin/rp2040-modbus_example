4f129b9d803606ede916ba6767a3ed6c2adf75a2
danjperron <danjperron@videotron.ca>
2023-08-28 15:53:25 -0400

    add unique ID

--- ModbusPico.hpp
+++ ModbusPico.hpp
@@ -3,7 +3,6 @@
 #include "modbus.hpp"
 #include <vector>
 
-
 class ModbusPico : public ModbusManager
 {
 public:
@@ -14,6 +13,14 @@ public:
     static uint8_t Coils[COILS_MAX];
     static uint8_t Inputs[INPUTS_MAX];
 
+    // holdin register add on  
+    // unique board ID register0 is bit 0..15 register3 is bit 48..64
+    static const uint16_t MB_COMMAND_UNIQUE_ID_REGISTER0=200;
+    static const uint16_t MB_COMMAND_UNIQUE_ID_REGISTER1=201;
+    static const uint16_t MB_COMMAND_UNIQUE_ID_REGISTER2=202;
+    static const uint16_t MB_COMMAND_UNIQUE_ID_REGISTER3=203;
+    uint16_t UniqueID[4];
+
     void mb_init(uint8_t slave_address, uint8_t uart_num,
              uint32_t baudrate, uint8_t data_bits, uint8_t stop_bits, uart_parity_t parity,
              uint8_t rx_pin, uint8_t tx_pin, uint8_t de_pin);
@@ -23,6 +30,7 @@ protected:
     uint8_t mb_write_multiple_coils(uint16_t start, uint8_t* values, uint16_t len);
     uint8_t mb_read_coil_status(uint16_t start, uint16_t count);
     uint8_t mb_read_input_status(uint16_t start, uint16_t count);
+    uint8_t mb_read_holding_register(uint16_t addr, uint16_t* reg);
 
 };
 
