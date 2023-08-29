903ef1fef737752defd0032b054c78e88d185937
danjperron <danjperron@videotron.ca>
2023-08-29 09:07:53 -0400

    fix delete array and max ds18b20 count

--- sensors/ds18b20.hpp
+++ sensors/ds18b20.hpp
@@ -11,6 +11,7 @@ class  ds18b20
   public:
       static bool debug;
       ds18b20(uint8_t gpioPin);
+      ~ds18b20();
       static const uint8_t sensorMax=10;
       rom_address_t sensorAddress[sensorMax];
       uint8_t count;
